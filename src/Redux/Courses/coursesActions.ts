46d307fe6eec068d92978262d586c512270e8f67
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-04 21:57:22 +0300

    Редукс

--- src/Redux/Courses/coursesActions.ts
+++ src/Redux/Courses/coursesActions.ts
@@ -1,12 +1,13 @@
 import {SET_COURSE, SET_CREATE_COURSE_MODAL} from "./actionTypes";
+import {Course} from "../../Models/Courses";
 
 
-export const setCreateCourseModal = (state) => ({
+export const setCreateCourseModal = (state: boolean) => ({
   type: SET_CREATE_COURSE_MODAL,
   payload: state
 });
 
-export const setCourse = (state) => ({
+export const setCourse = (state: Course) => ({
   type: SET_COURSE,
   payload: state
 });
--- src/Redux/Courses/coursesReducer.ts
+++ src/Redux/Courses/coursesReducer.ts
@@ -1,8 +1,9 @@
 import {INITIAL_COURSES} from './initialState';
 import {SET_COURSE, SET_CREATE_COURSE_MODAL} from "./actionTypes";
+import {ReducerAction} from "../ReducerAction";
 
 
-export const coursesReducer = (state = INITIAL_COURSES, action = {}) => {
+export const coursesReducer = (state = INITIAL_COURSES, action: ReducerAction) => {
   switch (action.type) {
     case SET_COURSE:
       return {...state, course: action.payload};
